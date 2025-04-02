6f23ebff16082a411f2bb961852be1f272e8132c
Hemen-Sarzali <hemen.sarzali@atea.no>
2025-04-02 13:58:04 +0200

    Corrected wrokingDir path for oidc module | addded ../

--- infrastructure/environments/dev/oidc/oidc_dev.tf
+++ infrastructure/environments/dev/oidc/oidc_dev.tf
@@ -1,5 +1,5 @@
 module "tfaz_oidc_dev" {
-  source                   = "../../modules/OIDC"
+  source                   = "../../../modules/OIDC"
   for_each                 = toset(var.gh_branches)
   application_display_name = data.azuread_application.tfazspn_gh_oidc.id
   gh_org_name              = var.gh_org_name
