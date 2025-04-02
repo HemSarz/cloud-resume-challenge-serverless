0a12ded77aecf39c19a95214af497b0e57a4e4d9
Hemen-Sarzali <hemen.sarzali@atea.no>
2025-04-02 11:13:13 +0200

    Removed backend config in backend.tf

--- infrastructure/environments/dev/oidc/backend.tf
+++ infrastructure/environments/dev/oidc/backend.tf
@@ -1,36 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "bckndrg"
-    storage_account_name = "stgbcknd"
-    container_name       = "bckndstatecont"
-    key                  = "tfstatebcknd"
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.23.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "3.2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy      = true
-      recover_soft_deleted_certificates = false
-    }
-  }
-}
-
-/*
-### Non backend provider block to validate .tf files locally
-
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -48,4 +16,5 @@ provider "azurerm" {
   subscription_id = "64208b73-267b-43b1-9bb1-649f128147e6"
 
 }
-*/
+
+provider "azuread" {}
