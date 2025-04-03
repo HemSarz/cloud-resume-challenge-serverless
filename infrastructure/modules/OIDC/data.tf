218dab7f972f5af6f2a62c55453655a7cd529bc0
Hemen-Sarzali <hemen.sarzali@atea.no>
2025-04-03 13:46:11 +0200

    Edited the resource name & appId vb

--- infrastructure/modules/OIDC/data.tf
+++ infrastructure/modules/OIDC/data.tf
@@ -1,7 +1,7 @@
-data "azurerm_client_config" "current" {
+data "azurerm_client_config" "current" {}
 
-}
+data "azuread_client_config" "current" {}
 
-data "azuread_application" "tfazapp" {
-  display_name = var.application_display_name
+data "azuread_application" "tfazspn" {
+  display_name = "tfazspn"
 }
