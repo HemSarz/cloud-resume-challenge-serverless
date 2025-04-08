6baf5842c59e213c5320f95c3f9e631b8d1e1efc
Hemen-Sarzali <hemen.sarzali@atea.no>
2025-04-08 11:40:56 +0200

    fix: src module contained foreach attri which caused the issue of replicating 'main' branch for each deployment instead of looping through the gh_branches vb

--- infrastructure/modules/OIDC/main.tf
+++ infrastructure/modules/OIDC/main.tf
@@ -1,8 +1,7 @@
 resource "azuread_application_federated_identity_credential" "branches" {
-  for_each       = toset(var.gh_branches)
   application_id = data.azuread_application.tfazspn.id
   display_name   = var.display_name
   audiences      = ["api://AzureADTokenExchange"]
   issuer         = "https://token.actions.githubusercontent.com"
-  subject        = "repo:${var.gh_org_name}/${var.gh_repo_name}:ref:refs/heads/${each.key}"
+  subject        = var.subject
 }
