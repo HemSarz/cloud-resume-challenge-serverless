bce1f1709572ab90a6d601efa08afe270eb22a28
Hemen-Sarzali <hemen.sarzali@atea.no>
2025-05-30 09:34:38 +0200

    fix: was using local provider block instead of backend

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,44 +1,23 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "bckndrg"
-#     storage_account_name = "stgbcknd"
-#     container_name       = "bckndstatecont"
-#     key                  = "tfstatebcknd"
-#   }
-
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = ">= 4.23.0"
-#     }
-#     azuread = {
-#       source  = "hashicorp/azuread"
-#       version = "3.2.0"
-#     }
-#   }
-# }
-
-# provider "azurerm" {
-#   features {
-#     key_vault {
-#       purge_soft_delete_on_destroy      = true
-#       recover_soft_deleted_certificates = false
-#     }
-#   }
-#   use_oidc = true
-# }
-
-
-### Non backend provider block to validate .tf files locally
-
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "bckndrg"
+    storage_account_name = "stgbcknd"
+    container_name       = "bckndstatecont"
+    key                  = "tfstatebcknd"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.23.0"
+      version = ">= 4.23.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.2.0"
     }
   }
 }
+
 provider "azurerm" {
   features {
     key_vault {
@@ -46,7 +25,5 @@ provider "azurerm" {
       recover_soft_deleted_certificates = false
     }
   }
-  subscription_id = "64208b73-267b-43b1-9bb1-649f128147e6"
-
+  use_oidc = true
 }
-
